a50890c418ac6efd361b08a7ac66dae4e2b44be0
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2023-02-07 15:50:57 +0000

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Sunjay Kalsi on 07/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
